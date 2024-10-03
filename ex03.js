84d8656f1966a58562ff312c928abc788b87e204
Phil <xuyuwei19940909@gmail.com>
2024-10-03 11:13:36 -0700

    modify ex03

--- ex03.js
+++ ex03.js
@@ -4,8 +4,9 @@ const _ = require('lodash');
 
 const numbers = [1, 3, 5, 7, 9];
 
-const filteredNumber = _.filter(numbers, num => num >= 7)
-
-const firstGreaterThanFive = _.head(filteredNumber)
+const firstGreaterThanFive = _.chain(numbers)
+    .filter(num => num >= 7)
+    .head()
+    .value()
 
 console.log(firstGreaterThanFive); // Expected output: 7
