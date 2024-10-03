035711f98260374209ac44db7b60c9a24781c57e
Phil <xuyuwei19940909@gmail.com>
2024-10-03 11:16:45 -0700

    ex05 done

--- ex05.js
+++ ex05.js
@@ -4,6 +4,6 @@ const _ = require('lodash');
 
 const numbers = [1, 2, 3, 4];
 
-// your code here
+const product = _.reduce(numbers, (acc, number) => acc * number)
 
 console.log(product); // Expected output: 24
